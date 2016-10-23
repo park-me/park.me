f69d2b918ffb1048153637fd90d0a980e51d32ca
Diego Figueroa <diego.fv94@gmail.com>
2016-10-23 02:24:20 -0400

    remove bootstrap, add materialize

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('parkMeApp', ['parkMeApp.auth', 'parkMeApp.admin', 'parkMeApp.constants',
-    'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap',
+    'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router',
     'validation.match'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
